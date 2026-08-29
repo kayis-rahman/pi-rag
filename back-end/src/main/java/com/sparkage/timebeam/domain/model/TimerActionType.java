9e5b99a4a89873714289a8feadf0e67db21ba85a
kayis-rahman <kaisbk1@gmail.com>
2026-08-29 17:57:47 +0100

    chore: remove legacy Spring backend

--- back-end/src/main/java/com/sparkage/timebeam/domain/model/TimerActionType.java
+++
@@ -1,9 +0,0 @@
-package com.sparkage.timebeam.domain.model;
-
-public enum TimerActionType {
-    START,
-    PAUSE,
-    RESET,
-    STOP,
-    ADVANCE
-}
--- back-end/src/main/resources/db/migration/V3__add_deleted_at_to_tasks.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE tasks ADD COLUMN deleted_at TIMESTAMPTZ;
-CREATE INDEX idx_tasks_user_deleted ON tasks(user_id, deleted_at);
